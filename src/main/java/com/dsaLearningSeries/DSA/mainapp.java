e1cefea54ff727e7f494bfda2807c302a36d699a
Kuldeep Kumar Kashyap <kuldeepkashyap0206@gmail.com>
2026-06-03 16:35:41 +0530

    Solved Problem 2496

--- src/main/java/com/dsaLearningSeries/DSA/mainapp.java
+++ src/main/java/com/dsaLearningSeries/DSA/mainapp.java
@@ -8,11 +8,16 @@ public class mainapp {
    public static void main(String[] args) {
       ArrayProblems demo = new ArrayProblems();
       int[] arr ={4,9,23,4};
-     //int [] newArr= demo.shuffle(arr,3);
+
+       String[] strs ={"1","01","001","0001"};
+       //int [] newArr= demo.shuffle(arr,3);
+
+        int maxLength= demo.maximumValue(strs);
 
 
+       System.out.println("Max Length "+maxLength);
        //System.out.println(demo.asteroidsDestroyed(19%3));
-        System.out.println(demo.minimumCost(arr));
+      //  System.out.println(demo.minimumCost(arr));
 
 
     }
