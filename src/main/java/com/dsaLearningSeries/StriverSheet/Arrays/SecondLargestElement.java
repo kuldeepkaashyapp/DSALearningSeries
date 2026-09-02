12f6d11b7c6430e36a25309882a7958ea3bc1979
Kuldeep Kumar Kashyap <kuldeepkashyap0206@gmail.com>
2026-09-02 23:36:52 +0530

    Stiver removeDuplicates

--- src/main/java/com/dsaLearningSeries/StriverSheet/Arrays/SecondLargestElement.java
+++ src/main/java/com/dsaLearningSeries/StriverSheet/Arrays/SecondLargestElement.java
@@ -1,5 +1,8 @@
 package com.dsaLearningSeries.StriverSheet.Arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SecondLargestElement {
 
 
@@ -41,10 +44,38 @@ public class SecondLargestElement {
     }
 
 
-    public  static void main(String[] args) {
-        int arr[]={8, 8, 7, 6, 5};
 
-        System.out.println(secondLargestElement(arr));
+    public static boolean isSorted(ArrayList<Integer> nums)  throws Exception{
+        boolean status=false;
+        for(int i =0; i <nums.size(); i++){
+
+            if(nums.get(i) >= nums.get(i + 1) && nums.get(i) <= nums.get(i + 1)){
+
+                status=true;
+
+            }else{
+
+                return false;
+            }
+
+
+
+        }
+
+        return status;
+        //your code goes here
+    }
+
+
+    public  static void main(String[] args) {
+        int [] arr= {
+1,2,3,4,5,6,7,8,9
+        };
+
+
+
+
+       // System.out.println(isSorted(ar));
     }
 
 
